662e3a3070eeb4404140364b6aa234467007fd47
Matthew Henschke <matthewhenschke98@gmail.com>
2020-01-18 09:44:07 -0500

    attempting to add future backend to portolio

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const PORT = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+  res.send("Hello from backend!");
+});
+
+// always do this step after route config or else it can override the routes
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "build", "index.html")); // create relative path, __dirname is current directory
+  });
+}
+
+app.listen(PORT, () => {
+  console.log(`server is running on port:${PORT}`);
+});
