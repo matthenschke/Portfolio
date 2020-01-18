be582d025f4f59445c287cc8a5cfa388e4609b08
Matthew Henschke <matthewhenschke98@gmail.com>
2020-01-18 13:25:58 -0500

    attempt to test api routes

--- server.js
+++ server.js
@@ -4,10 +4,6 @@ const path = require("path");
 
 const PORT = process.env.PORT || 8000;
 
-app.get("/hello", (req, res) => {
-  res.json("Hello from backend!");
-});
-
 // always do this step after route config or else it can override the routes
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("build"));
@@ -15,6 +11,9 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.join(__dirname, "build", "index.html")); // create relative path, __dirname is current directory
   });
 }
+app.get("/hello", (req, res) => {
+  res.json("Hello from backend!");
+});
 
 app.listen(PORT, () => {
   console.log(`server is running on port:${PORT}`);
